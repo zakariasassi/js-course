a4fdbc75ad7d6dcca707e9f3c5536566088b2bc0
zakaria sassi <king.herts.zeko@gmail.com>
2022-04-15 02:28:53 +0200

    ok

--- l-1/vars.js
+++ l-1/vars.js
@@ -2,7 +2,7 @@
 
 var name = "zakaria";
 var age =  26 ;
-
-
+var age = 19;
 console.log(name + age);
+
 
---
+++ l-1/vars2.js
@@ -0,0 +1,9 @@
+var username = "zakaria sassi";
+var age = 26 ;
+var job = "software eng";
+var skills = "web dev , mobile dev ";
+
+
+console.log(username + " " + age + " " + job + " " + skills );
+
+
