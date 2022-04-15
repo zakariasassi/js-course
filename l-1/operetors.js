78452691f2b33e5d251331d42ff071d8901f0f2e
zakaria sassi <king.herts.zeko@gmail.com>
2022-04-15 03:03:26 +0200

    ok

---
+++ l-1/operetors.js
@@ -0,0 +1,29 @@
+//+++++++++++++++++
+var man = 1900;
+var wife = 800;
+var sum = man + wife;
+console.log("your number is : " + sum);
+
+//- - - - - - - - - -
+var me = 1996;
+var year = 2022;
+
+var myage = year - me;
+console.log("my age is : " + myage);
+
+// ضرب 
+//* * * * * * *
+var n1 = 10;
+var n2 = 5;
+console.log( "the result is : " + n1 * n2);
+
+
+//                القسمة
+// => =>  ( / / / / / / / / / / )
+
+var val = 10000;
+var bor = 6;
+
+var result = val / bor ;  // float
+console.log (result)
+
---
+++ l-1/vars3.js
@@ -0,0 +1,13 @@
+var name = "asma";  // string 
+var age = 12 ; //integer => int
+var price = 8.5 ; //float
+var mychar = 'z'; // char
+var t = true; // this is boolin
+var f = false; // this is boolin
+
+
+console.log(name + age + price + mychar );
+
+console.log(t);
+console.log(f);
+
