f00072ffc17c1f665c8d6762a9d4a4bb10d3620e
zakaria sassi <king.herts.zeko@gmail.com>
2022-04-21 01:16:11 +0200

    ok

---
+++ l-4/obj.js
@@ -0,0 +1,38 @@
+
+
+let users = {
+    //    key          value
+        firstName: 'Zakaria',
+        lastName : 'sassi',
+        age :26,
+        isActive : false,
+        printName: function (){
+            console.log("this is zeko");
+        },
+
+        skils : [
+            "web devoper",
+            "mobile apps devolper",
+            "ui ux designer",
+            "content creator",
+            "graphic designer",
+            "web designer",
+          ],
+
+          books : {
+              java : 2,
+              js : 41,
+              html:2,
+
+          }
+
+}
+
+
+
+console.log(users.firstName + " " + users.lastName);
+console.log(users.skils[0]);
+console.log( users.books.java);
+users.printName();
+
+
